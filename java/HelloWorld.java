91fe9c477bcbb6e111270935858b785e207ff0f9
Zhang Yong <32609289+zyseeker@users.noreply.github.com>
2019-11-12 19:53:37 +0800

    Update HelloWorld.java

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
     public static void main(String args[]) {
         try {
             while (true) {
-                System.out.println("This to test java with maven: " + new java.util.Date().toString());
+                System.out.println("This to test java console application: " + new java.util.Date().toString());
                 Thread.sleep(3000);
             }
         } catch (Exception e) {
